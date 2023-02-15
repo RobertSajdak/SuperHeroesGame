d3c18d5033d742778869e37e6cfff7a486ee2190
RobertSajdak <robert.sajdak@onet.eu>
2023-02-15 11:55:18 +0100

    Dodaj pliki projektów.

---
+++ SuperHeroesGame/Knight.cs
@@ -0,0 +1,23 @@
+﻿namespace SuperHeroesGame
+{
+    class Knight : Hero
+    {
+        public Knight(string name, int fullhp, Colors color) : base(name, fullhp, color)
+        {
+        }
+
+        public override void DefaultAttack(Hero hero)
+        {
+            int hp = rnd.Next(50, 201);
+            hero.ActualHP -= hp;
+            Console.WriteLine($"\nGracz {Name} zadał {hp} punktów obrażeń graczowi {hero.Name}.");
+        }
+
+        public override void Heal()
+        {
+            int hp = rnd.Next(50, 101);
+            ActualHP += hp;
+            Console.WriteLine($"\nGracz {Name} uzdrowił się za {hp} punktów życia.");
+        }
+    }
+}
---
+++ SuperHeroesGame/Wizard.cs
@@ -0,0 +1,31 @@
+﻿namespace SuperHeroesGame
+{
+    class Wizard : Hero, ISpecialAttack
+    {
+        public Wizard(string name, int fullhp, Colors color) : base(name, fullhp, color)
+        {
+        }
+
+        public override void DefaultAttack(Hero hero)
+        {
+            int hp = rnd.Next(100, 151);
+            hero.ActualHP -= hp;
+            Console.WriteLine($"\nGracz {Name} zadał {hp} punktów obrażeń graczowi {hero.Name}.");
+        }
+
+        public override void Heal()
+        {
+            int hp = rnd.Next(100, 201);
+            ActualHP += hp;
+            Console.WriteLine($"\nGracz {Name} uzdrowił się za {hp} punktów życia.");
+        }
+
+        public void SpecialAttack(Hero hero)
+        {
+            int hp = rnd.Next(250, 301); 
+
+            hero.ActualHP -= hp;
+            Console.WriteLine($"\nAtak Specjalny! Gracz {Name} zadał {hp} punktów obrażeń graczowi {hero.Name}.");
+        }
+    }
+}
